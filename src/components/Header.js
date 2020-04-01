5ca72ac6d3a1224d61269cd26cdf8f8b5a71bf52
Carl Riis <carl.aagot.riis@gmail.com>
2020-04-01 21:36:54 +0200

    Mobile nav bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {useLocation, useHistory} from "react-router-dom";
+
+import menuIcon from "./../styling/res/burger_menu.svg";
 
 export default function Header(props) {
     const location = useLocation();
     const history = useHistory();
 
+    const [showMenu, setShowMenu] = useState(false);
+
+    const togleShowMenu = () => {
+       setShowMenu(!showMenu); 
+    }
+
+    const getShowMenu = () => {
+        if (showMenu) return "show-menu";
+        return "";
+    }
+    
     const getPageOn = (pathName) => {
         if(location.pathname === pathName) {
             return "page-on";
@@ -15,7 +28,8 @@ export default function Header(props) {
     return (
         <div className="header">
             <nav>
-                <ul>
+                <img src={menuIcon} className="menu-icon" onClick={togleShowMenu}/>
+                <ul className={getShowMenu()}>
                     <li className="logo">
                          <h1 className="logo"><span>Connect Dev io</span></h1>
                     </li>
